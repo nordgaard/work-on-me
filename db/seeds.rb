acd3615cbc86f0bcdf5d69fc214d3740674e8137
nordgaardgit config --global user.email “jenordgaard@gmail.com <jenordgaard@gmail.com>
2017-03-20 20:48:10 -0700

    init commit

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+# 10.times do 
+#     User.create!(
+#         :first_name => Faker::Name.first_name,
+#         :last_name => Faker::Name.last_name,
+#         :email => Faker::Internet.email,
+#         :phone => Faker::Number.number(10),
+#         :state => Faker::Address.state_abbr,
+#         :city => Faker::Address.city,
+#         :zip_code => Faker::Address.zip_code,
+#         :street => Faker::Address.street_address)    
+#     end
+
+
+puts "Done!"
